58732603a379874be51491fbe05da66ed3ef2e99
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-01 16:41:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
 
-int main(){
-  return 0;
-}
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+    int num = 0;
+    cin >> num;
+        if (num==1) {
+            cout << "numero non primo" << endl;
+        } else if (numeri_primi(num, num/2)) {
+            cout << "numero primo" << endl;
+        } else {
+            cout << "numero non primo" << endl;
+        }
+        return 0;
+    }
+
