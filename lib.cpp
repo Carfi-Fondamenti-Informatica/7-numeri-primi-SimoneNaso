d15a8598902eb08109da4bf258ee2c72759356d3
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-01 16:40:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+ bool numeri_primi (int a, int b) {
+     if (b==1) {
+       return true;
+    } else if (a%b==0) {
+         return false;
+     } else {
+        return numeri_primi(a, b-1);
+    }
+}
